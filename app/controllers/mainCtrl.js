3a674c722f4d6140df35b135cd5091f8b9471cb1
Dominika <d.k.krzysztofik@gmail.com>
2016-11-21 08:37:14 +0100

    +show shopping list +add new task +archive task +change user

--- app/controllers/mainCtrl.js
+++ app/controllers/mainCtrl.js
@@ -4,4 +4,30 @@
 angular.module('myApp')
     .controller('mainCtrl', ['$scope', '$rootScope', function ($scope, $rootScope) {
         $scope.basic = "Basic view";
+        $scope.archive = {};
+        $scope.users = ['zosia', 'kasia', 'konfacela'];
+        $scope.username = $scope.users[0];
+        $scope.shoppingList = {
+            zosia : ['mleko', 'ogórki', 'banany'],
+            kasia : ['mąka', 'pomidory', 'mleko'],
+            konfacela : ['orzechy']
+        };
+
+        $scope.moveToArchive = function (username, index) {
+            if($scope.archive[username] === undefined){
+                $scope.archive[username] = [];
+            }
+            $scope.archive[username].push($scope.shoppingList[username][index]);
+            $scope.shoppingList[username].splice(index, 1);
+        };
+
+        $scope.addToShoppingList = function (index) {
+            $scope.shoppingList[$scope.username].push($scope.archive[$scope.username][index]);
+            $scope.archive[$scope.username].splice(index, 1);
+        };
+
+        $scope.addNewTask = function () {
+            $scope.shoppingList[$scope.username].push($scope.task);
+            $scope.task = '';
+        }
     }]);
